271d0579a1fe954c8ae9981ad3c634ba47c90617
TrevonC <trevon.costa@georgebrown.ca>
2022-02-17 00:32:16 -0500

    Added Labels for number generation

--- LotteryNumbers/LotteryNumbers/ViewController.swift
+++ LotteryNumbers/LotteryNumbers/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numOne: UILabel!
+    @IBOutlet weak var numTwo: UILabel!
+    @IBOutlet weak var numThree: UILabel!
+    @IBOutlet weak var numFour: UILabel!
+    @IBOutlet weak var numFive: UILabel!
+    @IBOutlet weak var numSix: UILabel!
+    @IBOutlet weak var numSeven: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         header()
+        number()
         // Do any additional setup after loading the view.
     }
     
@@ -24,5 +33,24 @@ class ViewController: UIViewController {
         numbersView.layer.shadowOffset = CGSize(width: 5, height: 5)
         numbersView.layer.shadowRadius = 10.0
         numbersView.layer.shadowColor = UIColor.darkGray.cgColor
+    }
+    
+    // Rounding Each Label
+    func number(){
+        // Currently unaware on how to shorten this to clean it up. I'll put it on the backlog for now.
+        numOne.layer.masksToBounds = true
+        numOne.layer.cornerRadius = 12
+        numTwo.layer.masksToBounds = true
+        numTwo.layer.cornerRadius = 12
+        numThree.layer.masksToBounds = true
+        numThree.layer.cornerRadius = 12
+        numFour.layer.masksToBounds = true
+        numFour.layer.cornerRadius = 12
+        numFive.layer.masksToBounds = true
+        numFive.layer.cornerRadius = 12
+        numSix.layer.masksToBounds = true
+        numSix.layer.cornerRadius = 12
+        numSeven.layer.masksToBounds = true
+        numSeven.layer.cornerRadius = 12
     }
 }
